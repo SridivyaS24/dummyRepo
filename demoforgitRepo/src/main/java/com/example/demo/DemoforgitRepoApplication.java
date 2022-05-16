809e1e8289db33bb84ecd43b90f8229880780fdd
Mohan_krishna <mohankrishnamadanapu@gmail.com>
2022-05-16 12:30:49 +0530

    new Repo added

---
+++ demoforgitRepo/src/main/java/com/example/demo/DemoforgitRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoforgitRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoforgitRepoApplication.class, args);
+	}
+
+}
